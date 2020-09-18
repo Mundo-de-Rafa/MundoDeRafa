f57f01c10062e20a4dd9363e59ea90fa720c4941
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-18 11:04:19 -0300

    removing trailing spaces

--- MundoDeRafa/Config/AppDelegate.swift
+++ MundoDeRafa/Config/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        let rootViewController = OnboardingViewController()
+        let rootViewController = StoryController()
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
         return true
@@ -38,6 +38,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
--- MundoDeRafa/Controller/OnboardingViewController.swift
+++ MundoDeRafa/Controller/OnboardingViewController.swift
@@ -16,7 +16,6 @@ class OnboardingViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
