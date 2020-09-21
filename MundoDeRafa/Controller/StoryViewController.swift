e5476f454c2bd0058c72a64ea18cff0f60b002a9
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-21 10:40:18 -0300

    creating collection view and setting constraints

--- MundoDeRafa/Controller/StoryViewController.swift
+++ MundoDeRafa/Controller/StoryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StoryViewController: UIViewController {
+class StoryViewController: UIViewController, UICollectionViewDelegate {
     let backgroundView: UIImageView = {
         let backView = UIImageView()
         backView.image = UIImage(named: "Background")
@@ -16,9 +16,20 @@ class StoryViewController: UIViewController {
         return backView
     }()
     
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
+        collectionView.register(StoryCell.self, forCellWithReuseIdentifier: StoryCell.identifier)
+        collectionView.backgroundView = backgroundView
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpBackgroundView()
+        setUpCollectionView()
     }
     
     func setUpBackgroundView() {
@@ -29,7 +40,51 @@ class StoryViewController: UIViewController {
             backgroundView.rightAnchor.constraint(equalTo: view.rightAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-
+    }
+    
+    func setUpCollectionView() {
+        self.view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        
+    }
+    
+}
+extension StoryViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StoryCell.identifier, for: indexPath)
+        
+        if let cell = cell as? StoryCell {
+            cell.cardLabel.text = "Rafa se preparando para escola"
+            cell.cardImage.image = UIImage(named: "rafa_at_home")
+        }
+        
+        return cell
+        
+    }
+    
+    private func collectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let cellHeightConstant: CGFloat = 600
+        let cellWidthConstant: CGFloat = 400
+        layout.sectionInset = UIEdgeInsets(top: 138,
+                                           left: 88,
+                                           bottom: 0,
+                                           right: 72)
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = 42
+        layout.itemSize = CGSize(width:cellWidthConstant , height: cellHeightConstant)
+        
+        return layout
     }
     
 }
