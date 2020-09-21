8065b32e1a2c225262e58cd38aa4fa4fba571479
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-20 23:27:53 -0300

    creating new file for story view controller

---
+++ MundoDeRafa/Controller/StoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StoryViewController.swift
+//  MundoDeRafa
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 20/09/20.
+//  Copyright © 2020 Albert Rayneer. All rights reserved.
+//
+
+import UIKit
+
+class StoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
