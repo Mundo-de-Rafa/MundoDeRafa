91a006b52497b2a17fed9702cec8fb7ff48de064
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-21 10:38:51 -0300

    adding green secundary color

--- MundoDeRafa/Extensions/AppColors.swift
+++ MundoDeRafa/Extensions/AppColors.swift
@@ -16,6 +16,7 @@ extension UIColor {
     static let secondaryPurple = UIColor(red: 164/255, green: 134/255, blue: 213/255, alpha: 1)
     ///Stories buttons
     static let primaryGreen = UIColor(red: 4/255, green: 140/255, blue: 122/255, alpha: 1)
+    static let secondaryGreen = UIColor(red: 133/255, green: 220/255, blue: 207/255, alpha: 1)
     ///Used for interface texts and modal backgrounds
     static let backgroundWhite = UIColor(red: 251/255, green: 255/255, blue: 254/255, alpha: 1)
     
