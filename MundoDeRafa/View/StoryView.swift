2d124189ce15e47ec370f41d9194329005aaadc6
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-23 11:36:59 -0300

    adding new assets and solving conflict

--- MundoDeRafa/View/StoryView.swift
+++ MundoDeRafa/View/StoryView.swift
@@ -77,12 +77,8 @@ class StoryView: UIView {
     }
     
     @objc func didTapBackButton() {
-<<<<<<< HEAD
-        viewController?.backButtonAction()     
-=======
         viewController?.backButtonAction()
         backButton.playSoundIfNeeded(of: .click)
->>>>>>> e294a11718e6e4e987635bbf43f90b81f6c34a64
     }
     
     func setUpBackgroundView() {
