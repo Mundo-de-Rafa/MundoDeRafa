bccc23e56f214b586425b13a8ab438e55f78b4a7
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-23 09:05:54 -0300

    renaming assets and creating new file for view

---
+++ MundoDeRafa/View/StoryView.swift
@@ -0,0 +1,9 @@
+//
+//  StoryView.swift
+//  MundoDeRafa
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 23/09/20.
+//  Copyright © 2020 Albert Rayneer. All rights reserved.
+//
+
+import Foundation
