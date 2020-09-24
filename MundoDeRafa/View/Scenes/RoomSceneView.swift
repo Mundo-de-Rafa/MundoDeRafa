08f3665cb248f8994e658eb590f89400c5484eb2
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-24 15:59:23 -0300

    adding bedroom assets

---
+++ MundoDeRafa/View/Scenes/RoomSceneView.swift
@@ -0,0 +1,21 @@
+//
+//  RoomSceneView.swift
+//  MundoDeRafa
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 24/09/20.
+//  Copyright © 2020 Albert Rayneer. All rights reserved.
+//
+
+import UIKit
+
+class RoomSceneView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
