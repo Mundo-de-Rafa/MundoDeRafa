a99f248bd9e387d4501acd7d8afca35467332cdf
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-25 09:26:41 -0300

    adding view to controller

--- MundoDeRafa/Controller/Scenes/BedroomViewController.swift
+++ MundoDeRafa/Controller/Scenes/BedroomViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class BedroomViewController: UIViewController {
 
+    override func loadView() {
+        let bedroomView = BedroomView()
+        bedroomView.bedroomViewController = self
+        view = bedroomView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
