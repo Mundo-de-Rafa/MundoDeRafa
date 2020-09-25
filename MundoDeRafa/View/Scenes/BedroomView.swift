8d9aa155ddcd97ccecf5a5defa9cecc5bfb47f9f
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-25 09:24:53 -0300

    MundoDeRafa/View/Scenes/BedroomView.swift

---
+++ MundoDeRafa/View/Scenes/BedroomView.swift
@@ -0,0 +1,27 @@
+//
+//  RoomSceneView.swift
+//  MundoDeRafa
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 24/09/20.
+//  Copyright © 2020 Albert Rayneer. All rights reserved.
+//
+
+import UIKit
+
+class BedroomSceneView: UIView {
+
+    lazy var background: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "bedroom_background")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
