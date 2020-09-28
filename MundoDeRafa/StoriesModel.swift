05982a6b81c8a745e9c4075f4d876d32bb5962ad
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-28 11:01:38 -0300

    separating cell type in different files

---
+++ MundoDeRafa/StoriesModel.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  MundoDeRafa
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 28/09/20.
+//  Copyright © 2020 Albert Rayneer. All rights reserved.
+//
+
+import Foundation
