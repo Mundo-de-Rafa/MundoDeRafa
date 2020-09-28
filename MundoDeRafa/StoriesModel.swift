c44eb5d7ad08955b53e9dc3ef20bc0ae44e922fa
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-28 11:02:03 -0300

    adding stories model

--- MundoDeRafa/StoriesModel.swift
+++ MundoDeRafa/StoriesModel.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+
+enum CardState {
+    case locked
+    case unlocked
+}
+
+struct Card {
+    let title: String
+    let image: UIImage
+    let progress: Float
+    let state: CardState
+}
+
+
+var cards : [Card] = []
+var cardUnlocked = Card(title: "Rafa se preparando para escola", image: UIImage(named: "rafa_at_home") ?? UIImage(), progress: 0.4, state: .unlocked)
+
+let cardLocked = Card(title: "Bloqueado", image: UIImage(named: "locked") ?? UIImage(), progress: 0.0, state: .locked)
+
+
+
+
+
+
