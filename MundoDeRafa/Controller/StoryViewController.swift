36bdf00475ffd95dad78b3b2a27e36e5004ce19c
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2020-09-29 00:42:46 -0300

    adding animation to locked cell, sounds on click, showing different cells in collection view

--- MundoDeRafa/Controller/StoryViewController.swift
+++ MundoDeRafa/Controller/StoryViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class StoryViewController: UIViewController, UICollectionViewDelegate {
-    let cards : [Card] = [cardUnlocked, cardLocked]
-
+    let cards : [Card] = [firstCard, cardLocked, cardLocked, cardLocked]
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -25,7 +25,7 @@ class StoryViewController: UIViewController, UICollectionViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     func backButtonAction() {
@@ -39,39 +39,49 @@ extension StoryViewController : UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-       
+        
         switch cards[indexPath.row].state {
-            case .locked:
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StoryCellLocked.identifier, for: indexPath)
-                if let cell = cell as? StoryCellLocked {
-                    cell.cardLabel.text = cards[indexPath.item].title
-                    cell.cardImage.image = cards[indexPath.item].image
-                    cell.progressIndicator.progress = cards[indexPath.item].progress
-                }
-                return cell
+        case .locked:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StoryCellLocked.identifier, for: indexPath)
+                    if let cell = cell as? StoryCellLocked {
+                        cell.cardLabel.text = cards[indexPath.item].title
+                        cell.cardImage.image = cards[indexPath.item].image
+                        cell.progressIndicator.progress = cards[indexPath.item].progress
+                    }
+        
+                    return cell
+        case .unlocked:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StoryCellUnlocked.identifier, for: indexPath)
+            if let cell = cell as? StoryCellUnlocked {
+                cell.cardLabel.text = cards[indexPath.item].title
+                cell.cardImage.image = cards[indexPath.item].image
+                cell.progressIndicator.progress = cards[indexPath.item].progress
                 
-            case .unlocked:
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StoryCellUnlocked.identifier, for: indexPath)
-                if let cell = cell as? StoryCellUnlocked {
-                    cell.cardLabel.text = cards[indexPath.item].title
-                    cell.cardImage.image = cards[indexPath.item].image
-                    cell.progressIndicator.progress = cards[indexPath.item].progress
-
-                }
-                return cell
+            }
+            return cell
         }
-
-//        if let cell = cell as? StoryCell {
-//            cell.cardState = cards[indexPath.item].state
-//            cell.cardLabel.text = cards[indexPath.item].title
-//            cell.cardImage.image = cards[indexPath.item].image
-//            cell.progressIndicator.progress = cards[indexPath.item].progress
-//
-//        }
+    }
+    
+    func shakingAnimation() -> CAKeyframeAnimation {
+        // transform is a property of CAlayer which can rotate, translate and scale
+        let transformAnim  = CAKeyframeAnimation(keyPath:"transform")
+        //animation values from a position to another
+        // CATransform3DMakeRotation(angle , x vector, y vector, z vector)
+        // 2 values for initial position and final position
+        transformAnim.values  = [NSValue(caTransform3D: CATransform3DMakeRotation(0.04, 0, 0, 1)),NSValue(caTransform3D: CATransform3DMakeRotation(-0.04 , 0, 0, 1))]
+        transformAnim.duration  = 0.1
+        transformAnim.repeatCount = 3
+        return transformAnim
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        self.navigationController?.pushViewController(ScenesViewController(), animated: true)
+    
+        if cards[indexPath.row].state == .locked {
+            collectionView.cellForItem(at: indexPath)?.layer.add(shakingAnimation(), forKey: "transform")
+            SoundHelper.playSound(resource: "unsuccessfulAction")
+            
+        } else {
+            self.navigationController?.pushViewController(ScenesViewController(), animated: true)
+        }
     }
 }
